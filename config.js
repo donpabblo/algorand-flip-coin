fbbd464c2d3669d56e2130567824fea77cdb517c
Paolo Servillo <servill7@posteitaliane.it>
2022-06-17 13:57:25 +0200

    deleted mnemonic example

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 /* do not put password or any sensitive info here, done only for demo */
 const config = {
-  mnemonic: env.MNEMONIC || "put seed phrase here",
+  mnemonic: env.MNEMONIC,
   algodClientUrl: "https://testnet-algorand.api.purestake.io/ps2",
   algodClientPort: "",
   algodClientToken: "QWvOEenBjg1mkIsC9dPNZ9q9AsIPTtKXalswRv8u"
