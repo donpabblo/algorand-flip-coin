c5c53203a3e5be0ad44215c15a1242a89221b2bd
Paolo Servillo <servill7@posteitaliane.it>
2022-06-29 10:59:56 +0200

    build updated

--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  algodClientUrl: "https://testnet-algorand.api.purestake.io/ps2",
+  algodClientPort: "",
+  algodClientToken: "QWvOEenBjg1mkIsC9dPNZ9q9AsIPTtKXalswRv8u",
+  recipientAddress: "FVQID2AVLW7NRK6P3FLT27356TMQMGS53ONA3RJ25WOBUKRUJXEO2HOGCE",
+  algoConnectSettings: {
+    shouldSelectOneAccount: true,
+    openManager: false
+  }
 };
